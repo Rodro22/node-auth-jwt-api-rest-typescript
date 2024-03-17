9152114f489c874c4970b440c3977d0f8369bcf6
Rodro22 <rodrigoe.lezcano@gmail.com>
2024-03-17 12:33:54 -0300

    Proyecto con Node y otras cosas

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config()
+import express from 'express';
+import authRoutes from './routes/authRoutes'
+
+const app = express()
+
+app.use(express.json())
+
+//Routes
+
+app.use('/auth', authRoutes)
+
+//hacer una api rest de usuario
+
+
+export default app;
+
+
